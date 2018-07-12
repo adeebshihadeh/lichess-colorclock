14491a4ef2fc449aa3978f1b06962d6d5d69f5b4
quillford <quillford@users.noreply.github.com>
2018-07-11 21:37:52 -0700

    initial commit

---
+++ inject.js
@@ -0,0 +1,46 @@
+chrome.extension.sendMessage({}, function(response) {
+	var readyStateCheckInterval = setInterval(function() {
+  	if (document.readyState === "complete") {
+  		clearInterval(readyStateCheckInterval);
+
+      var timer;
+
+      if (document.querySelector(".header") &&
+        document.querySelector(".header").innerText.indexOf("now") &&
+        document.querySelectorAll(".time").length == 2) {
+        timer = setInterval(loop, 10);
+        console.log("active game detected");
+      } else {
+        console.log("no active game");
+      }
+
+      function loop() {
+        // TODO: check for aborted game too
+        if(document.querySelector(".result")) {
+          window.clearInterval(timer);
+          console.log("game ended");
+          document.querySelector("body").style.background = null;
+          return;
+        }
+
+        var opTime = getSeconds(document.querySelectorAll(".time")[0].innerText);
+        var time = getSeconds(document.querySelectorAll(".time")[1].innerText);
+
+        if (opTime < 5 || time < 5) {
+          document.querySelector("body").style.background = opTime < time ? "green" : "red";
+        } else {
+          document.querySelector("body").style.background = null;
+        }
+      }
+
+      function getSeconds(t) {
+        t = t.split(":").reverse();
+        var seconds = 0;
+        for(var i = t.length-1; i > 0; i--) {
+          seconds += parseFloat(t[i] * 60*i);
+        }
+        return seconds + parseFloat(t[0]);
+      }
+  	}
+	}, 10);
+});
